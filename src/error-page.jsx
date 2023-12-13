927905fd8b86598b48931170df7660504095d357
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-12-13 19:18:38 +0600

    404 page done

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -6,20 +6,47 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div
-      id="error-page"
-      className="flex flex-col w-1/2 items-center m-auto p-4"
-    >
-      <img
-        src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNDRwZTY5dXR3YWhvbHI5ZjgycWpnOGw4ZjZlY3VqZDQ1NDVmaDE4eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/UoeaPqYrimha6rdTFV/giphy.gif"
-        alt=""
-      />
-      <p>
-        Error Message: <i>{error.statusText || error.message}</i>
-      </p>
-      <button className="btn btn-accent">
-        <Link to="/">Go to Home</Link>
-      </button>
+    <div id="error-page">
+      <div id="page" class="site">
+        <section class="error-404">
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-lg-12 text-center">
+                <div class="error-logo">
+                  <div class="the-logo">
+                    <a href="index-2.html">
+                      <img src="images/logo-white.svg" alt />
+                    </a>
+                  </div>
+                </div>
+                <div class="page-content">
+                  <h1>404</h1>
+                  <h2>Sorry! Page Not Found!</h2>
+                  <p>
+                    Oops! The page you are looking for does not exist. Please
+                    return to the site’s homepage.
+                  </p>
+                  <form role="search" method="get" class="search-form">
+                    <input
+                      type="search"
+                      class="search-field"
+                      placeholder="SEARCH..."
+                    />
+                    <button type="submit" class="search-submit">
+                      <i class="ot-flaticon-search"></i>
+                    </button>
+                  </form>
+                  <div class="ot-button">
+                    <Link class="octf-btn" to="/">
+                      TAKE ME HOME
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
